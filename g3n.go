8dce647e28c089bd67221faedda529d26d24f944
thommil <thomthom.millet@gmail.com>
2019-03-14 00:28:21 +0100

    licence update

--- g3n.go
+++ g3n.go
@@ -1,6 +1,5 @@
+// Copyright (c) 2019 Thomas MILLET. All rights reserved.
 // Copyright 2016 The G3N Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
 
 package g3n
 
